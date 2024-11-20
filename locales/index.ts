52f0225a3b4358b9cad97b9184e89413929e2aad
TripeaceEmo <sam5601tw@gmail.com>
2024-11-20 21:29:46 +0800

    Update French

--- locales/index.ts
+++ locales/index.ts
@@ -3,8 +3,9 @@
 import en from './en/translation';
 import zh_tw from './zh_tw/translation';
 import zh_cn from './zh_cn/translation';
+import fr from './fr/translation';
 
-const LOCALES = { zh_tw, zh_cn, en };
+const LOCALES = { zh_tw, zh_cn, en, fr };
 
 export type Language = keyof typeof LOCALES;
 
